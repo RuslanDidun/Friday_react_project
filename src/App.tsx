27a97ead2140954606c509342823f145c535c083
Kano066 <79617423+Kano066@users.noreply.github.com>
2022-05-10 19:52:27 -0600

    project update

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './d1_main/m1_ui/App.css'
+
+function App() {
+    return (
+        <div className="App">
+            {/*<Header/>*/}
+            {/*<Main/>*/}
+        </div>
+    )
+}
+
+export default App
